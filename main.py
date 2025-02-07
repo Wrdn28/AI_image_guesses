98502a6d157c7dcbecec3bf84a2cb9399474e33c
Wrdn28 <ifloraxd@gmail.com>
2025-02-07 16:57:05 +0700

    refactor some error &  add image prediction
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ import numpy as np
 model = Ai.load_ai()
 window = tk.Tk()
 
-img = Image.new(mode="RGB", size=(500, 500), color=0) 
-label = tk.Label(window, textvariable=prediction) 
+last_point = (0, 0)
+line_color = (0, 0, 255)
+canvas_color = (173, 216, 230)
+img = Image.new(mode="RGB", size=(500, 500), color=canvas_color) 
+label = tk.Label(window)
 tkimage = ImageTk.PhotoImage(img)
 canvas = tk.Label(window, image=tkimage)
 canvas.pack()
 
 draw = ImageDraw.Draw(img)
 
-last_point = (0, 0)
-line_color = (0, 0, 255)
+
 prediction = tk.StringVar()
 label = tk.Label(window, textvariable=prediction) 
 
@@ -32,13 +34,13 @@ def draw_image(event):
     img_temp = img_temp.flatten()
     output = model.predict([img_temp])
     if (output[0] == 0):
-        prediction("Ini adalah gambar kotak")
+        prediction.set("Ini adalah gambar kotak")
     elif(output[0] == 1):
-        prediction("Ini adalah gambar lingkaran")
+        prediction.set("Ini adalah gambar lingkaran")
     elif(output[0] == 2):
-        prediction("Ini adalah gambar segitiga")
+        prediction.set("Ini adalah gambar segitiga")
     else:
-        prediction("Ini adalah gambar garislurus")
+        prediction.set("Ini adalah gambar Garis lurus")
     label.pack()
 
 def start_draw(event):
@@ -47,7 +49,7 @@ def start_draw(event):
 
 def reset_canvas(event):
     global tkimage, img, draw
-    img = Image.new(mode="RGB", size=(500, 500), color=0)
+    img = Image.new(mode="RGB", size=(500, 500), color=canvas_color)
     tkimage = ImageTk.PhotoImage(img)
     draw = ImageDraw.Draw(img)
     canvas['image'] = tkimage
@@ -55,7 +57,7 @@ def reset_canvas(event):
 
 kotak = 0
 lingkaran = 0
-garisluruslurus = 0
+garislurus = 0
 segitiga = 0
 
 def save_image(event):
@@ -79,6 +81,6 @@ window.bind("<ButtonPress-1>", start_draw)
 window.bind("<ButtonPress-3>", reset_canvas)
 window.bind("<Key>", save_image)
 
-label.pack
+label.pack()
 
 window.mainloop()
